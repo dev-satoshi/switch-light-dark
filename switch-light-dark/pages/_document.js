082e8457c12fb159d56426b1c9ff35d94510b492
satoshi <dev.satoshi.1016@gmail.com>
2023-04-14 08:14:02 +0900

    feat: メイン機能の実装

--- switch-light-dark/pages/_document.js
+++ switch-light-dark/pages/_document.js
@@ -1,13 +1,57 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='js'>
+        <Head></Head>
+        <body>
+          <script dangerouslySetInnerHTML={{
+            __html: themeInitializerScript,
+          }}>
+
+          </script>
+        </body>
         <Main />
         <NextScript />
-      </body>
-    </Html>
-  )
+      </Html>
+    );
+  }
 }
+
+const themeInitializerScript = `(function() {
+  ${setInitialColorMode.toString()}
+  setInitialColorMode();
+})()`;
+
+function setInitialColorMode() {
+  function getInitialColorMode() {
+    //ローカルストレージからthemeを取得する
+    const storedPreferenceMode = window.localStorage.getItem('theme');
+    const hasStoredPreference = typeof storedPreferenceMode === 'string';
+
+    if (hasStoredPreference) {
+      return storedPreferenceMode;
+    }
+
+    // 現在のpreference状態を確認する
+    const preference = window.matchMedia('(prefers-color-scheme): dark');
+    const hasMediaQueryPreference = typeof preference.matches === 'boolean';
+
+    if (hasMediaQueryPreference) {
+      return preference.matches ? 'dark' : 'light';
+    }
+
+    return 'light';
+  }
+
+  const currentColorMode = getInitialColorMode();
+  console.log(currentColorMode);
+
+  if (currentColorMode === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+}
+
+export default MyDocument;
--- switch-light-dark/pages/index.js
+++ switch-light-dark/pages/index.js
@@ -1,26 +1,51 @@
+import { useEffect, useState } from "react";
+
+
 export default function Home() {
+  const [darkTheme, setDarkTheme] = useState(undefined);
+
+  const handleToggle = (e) => {
+    setDarkTheme(e.target.checked);
+  };
+
+  useEffect(() => {
+    if (darkTheme !== undefined) {
+      if (darkTheme) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        window.localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.removeAttribute("data-theme");
+        window.localStorage.setItem("theme", "light");
+      }
+    }
+  }, [darkTheme]);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const initialColorValue = root.style.getPropertyValue(
+      "--initial-color-mode"
+    );
+    setDarkTheme(initialColorValue === "dark");
+  }, []);
+
   return (
     <div>
       <div className="container">
         <nav>
-          <div className="title">私のポートフォリオ</div>
+          <div className="title"></div>
           <div>
             <form action="#">
               <label className="switch">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  checked={darkTheme}
+                  onChange={handleToggle}
+                />
                 <span className="slider"></span>
               </label>
             </form>
           </div>
         </nav>
-        <section>
-          <div className="content">
-            <h1>Satoshiのポートフォリオ</h1>
-            <h3>ああああああああああああああああ</h3>
-            <p>あああああああああああああああああああああああああああああああああああああああああ</p>
-            <button className="primary-btn">お問い合わせ</button>
-          </div>
-        </section>
       </div>
     </div>
   );
